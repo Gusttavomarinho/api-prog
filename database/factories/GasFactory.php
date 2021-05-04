b4db155dbfe3a84dbd96b3c74c28fb428d585662
Gustavo Marinho <gusttavomarinho@gmail.com>
2021-05-04 19:56:31 -0300

    creat,model,migration , factory,seeder ,controller

---
+++ database/factories/GasFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\gas;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class GasFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = gas::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
