74dbfc05e47373e8bad952a405718fe5ef58c1cc
Gustavo Marinho <gusttavomarinho@gmail.com>
2021-05-04 20:31:26 -0300

    rota para insert api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\GasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Rotas GET
+
+Route::get('/gas',[GasController::class,'index']);
+
+
+//Rotas POST
+
+Route::post('/gas',[GasController::class,'create']);
